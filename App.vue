c4319be29219c734822ff9eb5b0d6e769a2a5bf9
azusa803 <118182495+azusa803@users.noreply.github.com>
2022-11-15 18:52:16 +0700

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,25 +1,67 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1 class="mb-4">List Contact</h1>
-      <hr>
-      <!-- //-------------------------------// -->
-      <customer-table v-bind:customers='customers' @edit-customer="editCustomer" @delete-customer="deleteCustomer"/>
-      <h1 class="mb-4">Form Input Contact</h1>
-      <hr>
-      <div class="" id="collapseExample">
-        <div class="card card-body">
-          <h2>Input Data Pelanggan</h2>
-          <hr>
-          <customer-form @add-customer="addCustomer"/>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container">
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <router-link class="nav-link" 
+                onclick="document.getElementById('list').style.visibility='visible';
+                document.getElementById('list').style.display='block';
+                document.getElementById('add').style.visibility='none';
+                document.getElementById('add').style.display='none';">
+                LIST CONTACT
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link"
+                onclick="document.getElementById('add').style.visibility='visible';
+                document.getElementById('add').style.display='block';
+                document.getElementById('list').style.visibility='none';
+                document.getElementById('list').style.display='none';">
+                ADD CONTACT</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <router-view> </router-view>
+      <div id="list">
+        <h1 class="mb-4">List Contact</h1>
+        <hr>
+        <!-- //-------------------------------// -->
+        <customer-table v-bind:customers='customers' @edit-customer="editCustomer" @delete-customer="deleteCustomer"/>
+      </div>
+      <div id="add" class="block">
+        <h1 class="mb-4">Form Input Contact</h1>
+        <hr>
+        <div class="" id="collapseExample">
+          <div class="card card-body">
+            <h2>Input Data Pelanggan</h2>
+            <hr>
+            <customer-form @add-customer="addCustomer"/>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
+<!--  <script src="unpkg.com/vue"></script> -->
 <script>
+
+// const routes = [
+//   {path:'/', component: CustomerTable},
+//   {path:'/add', component: CustomerForm}
+// ]
+// const router = new VueRouter({
+//   routes
+// })
+// new Vue({
+//   el: "#app",
+//   router
+// });
 
 import CustomerTable from '@/components/CustomerTable.vue'
 import CustomerForm from '@/components/CustomerForm.vue'
@@ -83,5 +125,9 @@ export default {
   text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+.block {
+  display: none;
+  visibility: hidden;
 }
 </style>
